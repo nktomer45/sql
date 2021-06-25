275b9d63413279a3b4d58619c68f91d616454d48
neeraj kumar <60112018+nktomer45@users.noreply.github.com>
2021-06-25 11:28:06 +0530

    Update create database.sql

--- create database.sql
+++ create database.sql
@@ -5,11 +5,3 @@ create table students(
 id int,
 name varchar(50));
 
-
-insert into students (id,name)values(1,'neeraj'),(2,'lalit'),(3,'kapli')
-
-----update----
-update students set name='vivek' where id=3
-
-------- delete----
-delete from students where id=3
